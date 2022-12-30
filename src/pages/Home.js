e7012479812e59805740ec5073d1f8ee47bfb557
brian nelson <bn3l10@gmail.com>
2022-12-30 02:28:20 -0500

    added hero, fonts, global styles, setup pages

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import AboutSection from '../components/AboutSection';
+// import ServicesSection from '../components/ServicesSection';
+// import ProjectsSection from '../components/ProjectsSection';
+// import ContactBanner from '../components/ContactBanner';
+import Hero from '../components/Hero';
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      {/* <AboutSection />
+      <ServicesSection />
+      <ProjectsSection />
+      <ContactBanner /> */}
+    </div>
+  );
+}
